9604a29c9e4b87a71d25a784a040940ba1efddfd
github username <github email address>
2017-10-16 19:07:52 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,34 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    key_reference_array = []
+    output = []
+    output_keys = []
+    arguments.each {|argument| key_reference_array << argument}
+    key_reference_array
+
+    each do |animal, country|
+      if key_reference_array.include? country
+        output << animal.to_s
+        output_keys << animal
+      end
+    end
+    output_keys
+    #output
   end
 end
+
+=begin
+let(:animals)
+{
+  {
+    "sugar glider"=>"Australia",
+    "aye-aye"=> "Madagascar",
+    "red-footed tortoise"=>"Panama",
+    "kangaroo"=> "Australia",
+    "tomato frog"=>"Madagascar",
+    "koala"=>"Australia"
+  }
+}
+=end
