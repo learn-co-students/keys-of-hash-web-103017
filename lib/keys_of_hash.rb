96b71f5ef7c967e6ed7d7ae6a54de869926d7872
github username <github email address>
2017-10-21 11:29:37 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(arguments, *anything_else)
+    new_array = []
+    self.each do |key, value|
+      #puts key
+      if value == arguments || value == anything_else[0]
+        new_array.push(key)
+      end
+
+    end
+    return new_array
   end
 end
+animal = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"}
+puts animal.keys_of('Panama', 'Australia')
